1a6d634a91a62976d6543953b69781a7750bf14e
abelmaniquis <abelmaniquis@gmail.com>
2016-04-28 21:06:44 +0000

    alerting user

--- js/app.js
+++ js/app.js
@@ -1,102 +1,95 @@
-
-var guess,
-number,
-pastguesses = [],
-input
-
 $(document).ready(function(){
-	
+    var guess,  //user's guess
+    tries = 0,
+    guesses = [],   //list of the past guesses
+    actualNum = parseInt((Math.random()*100),10);
+   // userinput,
+   // tries = 0 //count the number of tries
+    
+    
+/*-------Instructions-----------------------------------------------------------------------------------------*/
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
+		$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
   	});
 
   	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
+    	$(".close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-  	
-  	//Start new game
-  	
- //When a page loads, Javascript should start a new game. 	
+/*--------------------------------------------------------------------------------------------------------------*/
+        $(".new").click()
+        $("#guessButton").click(function(){
+ 	        var guess=$('#userGuess').val();
+ 	        checkuserinput(guess);
+  	        alert(evaluate(guess));
+  	        trackguesses(guess);
+  	});
+
+
 function newGame(){
+    resetVariables();
+}
+function getGuess(){
+    guess = $("#userGuess").val();
 }
 
-function getInput(){
-    guess = input.val();
-}
-
-//A secret number between 1 and 100 should be generated 
-function newNumber(){
-    var number = parseInt((Math.random()*100),10);
-}
-
-//The user should get feedback for each guest- if it was too low or too high
-//This should appear in div#feedback
-//When the page loads, the text in this field is "Make your Guess!"
-function feedback(){
-    var diff = guess - number;
-        if(diff == 0)
-        {
-            return("correct!");
-        }
-        else if(diff <= 10)
-        {
-            return("burning");
-        }
-        else if(diff <= 20)
-        {
-            return("hot");   
-        }
-        else if(diff <= 30)
-        {
-            return("warm");
-        }
-        else if(diff <= 40)
-        {
-            return("cool");
-        }
-        else if(diff < 50)
-        {
-            return("cold");
-        }
-        else if(diff >= 50)
-        {
-            return("freezing");
-        }
-}	
-
-//Track how many guesses the user has made.
-function tracker(){
+function evaluate(guess){
+    var diff = Math.abs(actualNum - guess);
     
-}
-
-//Supply users with a list of numbers they have guessed so far
-//Add as <li> to ul#guessList
-function list(){
-    
-}
-
-//You'll need to ensure that users provide valid inputs. 
-//If the user provides a blank guess, they will be prompted to supply a valid input
-function checkvalidinput(){
-    if(parseInt(guess,10) !== guess){
-        alert('please input a number');
+    if(diff == 0)
+    {
+        return("correct!");
     }
-    else if((guess < 0)||(guess > 100)){
-        alert('please input a number between 0 and 100');
+    else if(diff <= 10)
+    {
+         return("burning");
+    }
+    else if(diff <= 20)
+    {
+        return("hot");
+    }
+    else if(diff <= 30)
+    {
+        return("warm");
+    }
+    else if(diff <= 40)
+    {
+        return("cool");
+    }
+    else if(diff < 50)
+    {
+        return("cold");
+    }
+    else if(diff >= 50)
+    {
+        return('freezing');
     }
 }
 
-//Once button is clicked, must start a new game.
-//This function might not be needed
-function startnewGame(){
-    
+function resetVariables(){
+    tries = 0;
+    guesses = [];
 }
 
+function newGame(){
+    resetVariables();
+    alert(tries);
+}
+ 
+function trackguesses(player_guess){
+    guesses.push(player_guess);
+    console.log(guesses);
+}
 
+function checkuserinput(player_guess){
+    if(player_guess < 0){
+        alert("Please give me a value greater than 0");
+    }
+    if(guesses.indexOf(player_guess) > -1){
+        alert("you have already guessed that nmber");
+    }
+}
 
 });
-
-
